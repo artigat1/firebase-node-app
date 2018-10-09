3e78dca96a68500cfc99208b1dab46eca6454f92
Steve Doggett <artigat1@users.noreply.github.com>
2018-10-09 15:15:54 +0100

    Connect to Firestore to get the facts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -15,28 +15,16 @@ db.settings({
     timestampsInSnapshots: true
 });
 
-async function setFacts() {
-    await db.collection('facts').add({
-        text: 'The sun is hot'
-    });
-    console.log('added fact 1');
-    await db.collection('facts').add({
-        text: 'Fish can swim'
-    });
-    console.log('added fact 2');
-    await db.collection('facts').add({
-        text: 'Birds can fly'
-    });
-    console.log('added fact 3');
-    await db.collection('facts').add({
-        text: 'Xmas is in December'
-    });
-    console.log('added fact 4');
-}
-
 function getFacts() {
-    const ref = firebaseApp.database().ref('facts');
-    return ref.once('value').then(snap => snap.val());
+    return db.collection('facts')
+        .get()
+        .then(snapshot => {
+            const facts = [];
+            snapshot.forEach(fact => {
+                facts.push(fact.data());
+            });
+            return facts;
+        });
 }
 
 const server = express();
@@ -44,21 +32,22 @@ server.engine('hbs', engines.handlebars);
 server.set('views', './views');
 server.set('view engine', 'hbs');
 
-server.get('/', async (request, response) => {
+server.get('/', (request, response) => {
     response.set('Cache-Control', 'public, max-age=300, s-maxage=600');
-    const facts = await getFacts();
-    response.render('index', { facts });
+    getFacts()
+        .then(facts => {
+            response.render('index', { facts });
+        })
+        .catch(reason => console.error(reason));
 });
 
 server.get('/facts.json', async (request, response) => {
     response.set('Cache-Control', 'public, max-age=300, s-maxage=600');
-    const facts = ['the sun is hot', 'fish can swim', 'birds can fly'];
-    response.json(facts);
-});
-
-server.get('/init', async (request, response) => {
-    await setFacts();
-    return response.status(200).send({});
+    getFacts()
+        .then(facts => {
+            response.json(facts);
+        })
+        .catch(reason => console.error(reason));
 });
 
 // Start writing Firebase Functions
